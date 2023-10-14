c619a1940923651d87a82c9d6528984abcebcfd6
mentorvikash <mentorvikash@gmail.com>
2023-10-14 10:11:02 +0530

    event listner with counter app

--- day1-hello-world/src/App.vue
+++ day1-hello-world/src/App.vue
@@ -1,5 +1,14 @@
 <template>
-  <h1>Event Lisners</h1>
+  <h1>Event Lisners with Counter App</h1>
+  <h2>{{ count }}</h2>
+  <!-- <div class="button-parent">
+    <button v-on:click="count += 1">Increment</button>
+    <button v-on:click="count -= 1">Decrement</button>
+  </div> -->
+  <div class="button-parent">
+    <button v-on:click="decrement">Decrement</button>
+    <button v-on:click="increment">Increment</button>
+  </div>
 </template>
 
 <script>
@@ -7,11 +16,18 @@ export default {
   name: "App",
   data() {
     return {
-
+      count: 0
     }
   },
   methods: {
-
+    increment() {
+      this.count = this.count + 1;
+    },
+    decrement() {
+      if (this.count != 0) {
+        this.count -= 1
+      }
+    }
   }
 }
 </script>
@@ -22,5 +38,26 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.button-parent {
+  display: flex;
+  padding: 10px;
+  background-color: gainsboro;
+  justify-content: center;
+  gap: 12px;
+}
+
+.button-parent button {
+  padding: 5px;
+  color: white;
+  background: blue;
+  border: "1px solid black";
+  border-radius: 5px;
+
+  &:hover {
+    color: black;
+    background: white
+  }
 }
 </style>
