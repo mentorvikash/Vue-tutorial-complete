8ba92b71f7fe02f6a04c734dce01cc0905021aca
mentorvikash <mentorvikash@gmail.com>
2023-10-15 15:54:47 +0530

    validating emited events

--- vue-with-components/src/components/popup/popupElement.vue
+++ vue-with-components/src/components/popup/popupElement.vue
@@ -1,15 +1,34 @@
 <template>
     <div>
         <h2>PopUp Element is here</h2>
-        <button @click="$emit('close', 'vikash')">Close Button</button>
+        <input type="text" v-model="username" />
+        <!-- <button @click="$emit('close', 'vikash')">Close Button</button> -->
+        <button @click="$emit('close', username)">Close Button</button>
+
     </div>
 </template>
 
 <script>
 export default {
     name: 'PopupElement',
-    emits: ['close']
+    data() {
+        return {
+            username: '',
+        }
+    },
+    // emits: ['close']
+    emits: {
+        close(name) {
+            if (!name) {
+                return false
+            } else {
+                return true
+            }
+        }
+    }
 }
 </script>
 
 <style scoped></style>
+
+<!-- We can also add validation with to what action to be emit and what value to be passed -->
