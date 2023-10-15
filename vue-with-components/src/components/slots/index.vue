6af8c82a277e315b49ffe3a3d860f2dd5c9f799b
mentorvikash <mentorvikash@gmail.com>
2023-10-15 17:03:46 +0530

    add some text to test

--- vue-with-components/src/components/slots/index.vue
+++ vue-with-components/src/components/slots/index.vue
@@ -2,7 +2,7 @@
     <div class="main">
         <CardElement>Card Content 1</CardElement>
         <CardElement>
-            <h1>My New Heading</h1>
+            <h1>My New Heading add text</h1>
         </CardElement>
         <CardElement>
             <img src="https://image.khaleejtimes.com/?uuid=c6164f6d-a0a6-5492-96c9-395b0455f1cf&function=cropresize&type=preview&source=false&q=75&crop_w=0.99999&crop_h=0.74971&x=0&y=0&width=1200&height=675"
