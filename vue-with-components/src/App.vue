e049f31e0e51afb0c06071c896ae4815c6d7f05d
mentorvikash <mentorvikash@gmail.com>
2023-10-15 13:48:23 +0530

    create component and pass props

---
+++ vue-with-components/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <LetGreet name="Amar" fruit="Mango" />
+  <LetGreet name="Rajat" fruit="Papaya" />
+  <LetGreet name="Vishal" fruit="Orange" />
+  <LetGreet :name="name" :fruit="fruit" />
+</template>
+
+<script>
+import LetGreet from './components/greet.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      name: 'Revive',
+      fruit: 'Coding'
+    }
+  },
+  components: {
+    LetGreet
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-with-components/src/components/greet.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Hello {{ name }}, I know you like {{ fruit }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LetGreet",
+  props: ['name', 'fruit']
+}
+</script>
+
+<style scoped></style>
